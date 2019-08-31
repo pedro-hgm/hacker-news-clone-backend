8a93d50ace598c6449e73497331bb83e5870f1d4
Pedro Henrique <pedro.hgm30@gmail.com>
2019-08-31 09:41:58 -0300

    added: rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -21,6 +21,14 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # rack-cors config
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "localhost:3000, https://hackernewsvue.netlify.com/"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
